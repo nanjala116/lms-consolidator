007b0cac210e53b37727ca09e9601b537b005832
nanjala116 <nanjalaliz14@gmail.com>
2025-05-18 18:40:00 +0300

    ft(config):settings
    - config secret key (.env)

--- lms/settings.py
+++ lms/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0qn0q&42up7zl7791-$ugja4n4k0$ofv31a)s97fi7kh8(9+5&'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
