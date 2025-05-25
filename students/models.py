f1b40c798ca1db16339b2b9b305783e3a2261528
maisr <maisr@DESKTOP-8T8PQVK>
2025-05-25 14:52:58 +0200

    Updated models with required fields and relations; fixed defaults and migrations

--- students/models.py
+++ students/models.py
@@ -1,13 +1,8 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from professors.models import Professor
 
 class PhDStudent(models.Model):
     name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
+    title = models.CharField(max_length=10)  # e.g., Mr., Ms., Dr.
     supervisor = models.ForeignKey(Professor, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
+    research_topic = models.CharField(max_length=255)
