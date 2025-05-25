cbda6c2af57225f8dc016e3ced6dc103b062ea49
maisr <maisr@DESKTOP-8T8PQVK>
2025-05-25 14:52:58 +0200

    Updated models with required fields and relations; fixed defaults and migrations

--- courses/models.py
+++ courses/models.py
@@ -1,13 +1,15 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from professors.models import Professor
 
 class Course(models.Model):
-    title = models.CharField(max_length=100)
-    code = models.CharField(max_length=20, unique=True)
-    instructor = models.ForeignKey(Professor, on_delete=models.CASCADE)
+    code = models.CharField(max_length=100, unique=True)  # e.g., course-v1:GermanUDS+PRP_webbasics+2024_1
+    name = models.CharField(max_length=200)  # Course title
+    description = models.TextField(default='No description yet')
+    start_date = models.DateField()
+    format = models.CharField(max_length=50, default='Self-paced')
+
+    professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
+    
 
     def __str__(self):
-        return self.title
+        return self.name
--- professors/models.py
+++ professors/models.py
@@ -1,13 +1,8 @@
 from django.db import models
 
-# Create your models here.
-
-from django.db import models
-
 class Professor(models.Model):
     name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    department = models.CharField(max_length=100)
+    title = models.CharField(max_length=20)  # e.g., Dr., Prof.
 
     def __str__(self):
-        return self.name
+        return f"{self.title} {self.name}"
